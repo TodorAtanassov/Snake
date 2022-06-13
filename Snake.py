ae79336ac634907d53a5c738df7b1890ec2a257a
TodorAtanassov <todoratanasov432@gmail.com>
2022-06-13 15:22:49 +0300

    Sn

--- Snake.py
+++ Snake.py
@@ -2,9 +2,3 @@ import pygame
 import numpy
 import random
 import math
-pygame.init()
-clock = pygame.time.Clock()
-screen = pygame.display.set_mode((500, 500))
-grid = numpy.zeros((20, 20), dtype="int8")
-for i in range(4):
-    grid[7][2+i] = 2
