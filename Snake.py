481b4abf1bd098c71e43a3dff3debd1af2f84cee
TodorAtanassov <todoratanasov432@gmail.com>
2022-06-13 15:22:37 +0300

    Sn

--- Snake.py
+++ Snake.py
@@ -1,3 +1,10 @@
-import turtle
+import pygame
+import numpy
 import random
-import time
+import math
+pygame.init()
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((500, 500))
+grid = numpy.zeros((20, 20), dtype="int8")
+for i in range(4):
+    grid[7][2+i] = 2
