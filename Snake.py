e236d299c1857dfa25232441cc853150e08c01b4
TodorAtanassov <todoratanasov432@gmail.com>
2022-06-14 20:39:45 +0300

    Sn

--- Snake.py
+++ Snake.py
@@ -1,4 +1,12 @@
+import turtle
+
 import pygame
 import numpy
 import random
 import math
+
+screen = turtle.Screen()
+screen.title('Snake')
+screen.setup(width=900, height=900)
+screen.tracer(0)
+turtle.bgcolor('turquoise')
