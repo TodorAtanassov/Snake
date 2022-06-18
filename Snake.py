03aec8c61f2dccc825ea1d218ccb50e59e377bf5
TodorAtanassov <todoratanasov432@gmail.com>
2022-06-18 20:42:18 +0300

    Sn

--- Snake.py
+++ Snake.py
@@ -82,9 +82,18 @@ def snake_move():
         x = snake.xcor()
         snake.setx(x + 20)
 
-
+score = 0
+delay = 0
 screen.listen()
 screen.onkeypress(snake_go_up, "Up")
 screen.onkeypress(snake_go_down, "Down")
 screen.onkeypress(snake_go_left, "Left")
 screen.onkeypress(snake_go_right, "Right")
+if snake.distance(fruit)< 20:
+                x = random.randint(-290,270)
+                y = random.randint(-240,240)
+                fruit.goto(x,y)
+                scoring.clear()
+                score+= 1
+                scoring.write("Score:{}".format(score),align="center",font=("Courier",24,"bold"))
+                delay-= 0.001
