4fb1761865df03dd943125b78f0fe28a4a377e68
TodorAtanassov <todoratanasov432@gmail.com>
2022-06-18 20:43:21 +0300

    Sn

--- Snake.py
+++ Snake.py
@@ -98,3 +98,19 @@ if snake.distance(fruit) < 20:
     score += 1
     scoring.write("Score:{}".format(score), align="center", font=("Courier", 24, "bold"))
     delay -= 0.001
+new_fruit = turtle.Turtle()
+new_fruit.speed(0)
+new_fruit.shape('square')
+new_fruit.color('red')
+new_fruit.penup()
+old_fruit.append(new_fruit)
+for index in range(len(old_fruit) - 1, 0, -1):
+    a = old_fruit[index - 1].xcor()
+    b = old_fruit[index - 1].ycor()
+    old_fruit[index].goto(a, b)
+
+if len(old_fruit) > 0:
+    a = snake.xcor()
+    b = snake.ycor()
+    old_fruit[0].goto(a, b)
+snake_move()
